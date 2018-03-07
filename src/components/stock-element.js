9ebe71d2078dda70e3957c68cf5d35ebb562cac6
atomcorp <thomasmaxwellsmith@gmail.com>
2018-03-07 22:56:08 +0000

    forced length with fixed characters

--- src/components/stock-element.js
+++ src/components/stock-element.js
@@ -5,23 +5,36 @@
 import { StockType, StockElementType } from './flow-types';
 
 export const stockElement = (stock: StockType): StockElementType => {
+  const { change, percentage, price, title, id} = stock;
   const el = document.createElement('div');
   el.classList.add('stock');
-  const { change, percentage, price, title, id} = stock;
   Number(change) > 0
     ? el.classList.add('stock--up')
     : el.classList.add('stock--down');
   el.id = id;
-  el.appendChild(stockItemElement(title, 'stock__title'));
-  el.appendChild(stockItemElement(change, 'stock__change'));
-  el.appendChild(stockItemElement(price, 'stock__price'));
-  el.appendChild(stockItemElement(percentage, 'stock__percentage'));
+  el.appendChild(
+    stockItemElement(evenStrings([title, change], 14), 'stock__top')
+  );
+  el.appendChild(
+    stockItemElement(evenStrings([price, percentage], 14), 'stock__bottom')
+  );
+  // el.appendChild(stockItemElement(price, 'stock__price'));
+  // el.appendChild(stockItemElement(percentage, 'stock__percentage'));
   return el;
 };
 
 const stockItemElement = (text: string, className: string) => {
   const el = document.createElement('div');
-  el.innerText = text;
+  el.innerHTML = text;
   el.classList.add(className);
   return el;
+};
+
+const evenStrings = (strings: Array<string>, count: number) => {
+  let difference = count - (strings[0].length + strings[1].length);
+  if (difference < 0) {
+    difference = 0;
+  }
+  const space = Array.from(Array(difference)).map(() => '&nbsp;').join('');
+  return [...strings[0], ...space, ...strings[1]].join('');
 };
